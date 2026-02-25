493056fb0859a598bee80f9698f9f8fe0a29afcf
0x416e746f6e <anton@northernforest.nl>
2026-02-25 08:53:24 +0100

    fix: only unescape apostrophes inside strings

--- processor/sns.go
+++ processor/sns.go
@@ -19,16 +19,23 @@ func (p *Processor) ProcessSnsEvent(ctx context.Context, event events.SNSEvent) 
 
 	errs := []error{}
 	for _, r := range event.Records {
+		raw := []byte(r.SNS.Message)
 		m := &types.AlertmanagerMessage{}
-		message := strings.ReplaceAll(r.SNS.Message, "\\'", "'")
-		if err := json.Unmarshal([]byte(message), m); err != nil {
+
+		err := json.Unmarshal(raw, m)
+		if err != nil && err.Error() == "invalid character '\\'' in string escape code" {
+			raw = sanitiseJsEscapedApostrophes(raw)
+			err = json.Unmarshal(raw, m)
+		}
+		if err != nil {
 			l.Error("Error un-marshalling message",
-				zap.String("message", strings.ReplaceAll(message, "\n", " ")),
+				zap.String("message", strings.ReplaceAll(r.SNS.Message, "\n", " ")),
 				zap.Error(err),
 			)
 			errs = append(errs, err)
 			continue
 		}
+
 		if err := p.processMessage(ctx, r.SNS.TopicArn, m); err != nil {
 			errs = append(errs, err)
 		}
@@ -55,4 +62,47 @@ func (p *Processor) ProcessSnsEvent(ctx context.Context, event events.SNSEvent) 
 		}
 	}
 	return errors.Join(errs...)
+}
+
+// sanitiseJsEscapedApostrophes replaces escaped apostrophes (\') for just
+// apostrophes (') in the byte slice so that we could try to parse strings
+// that were processes by `js` function of AMP's alertmanager template engine
+func sanitiseJsEscapedApostrophes(input []byte) []byte {
+	res := make([]byte, 0, len(input))
+
+	var inString, sawEscape bool
+
+	for _, char := range input {
+		if !inString {
+			if char == '"' {
+				inString = true
+			}
+			res = append(res, char)
+			continue
+		}
+
+		// in string
+
+		if sawEscape {
+			if char == '\'' {
+				res = append(res, '\'') // substitute \' with ' inside strings
+			} else {
+				res = append(res, '\\', char)
+			}
+			sawEscape = false
+			continue
+		}
+
+		switch char {
+		case '\\':
+			sawEscape = true
+		case '"':
+			inString = false
+			res = append(res, char)
+		default:
+			res = append(res, char)
+		}
+	}
+
+	return res
 }
